159a1903741b2a00a553068f426d3e0d2bfc2e13
Joel de Santos <joeldesantos@gmail.com>
2018-07-05 12:09:15 -0500

    project structure created

---
+++ src/properties.py
@@ -0,0 +1,7 @@
+class Properties(object):
+	"""docstring for Properties"""
+	def __init__(self, arg):
+		super(Properties, self).__init__()
+		self.INPUT_FILE = arg[0]
+		self.OUTPUT_FILE = arg[1]
+		
